acebb3422299a2ea8a163bc9688aeabd9833de79
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-01-12 16:50:06 -0700

    fixes uninstalls semantics to respect restarts and propagates autorevert changes to FE

--- agent/src/Handler/Types/Apps.hs
+++ agent/src/Handler/Types/Apps.hs
@@ -14,6 +14,7 @@ import           Lib.Types.Core
 import           Lib.Types.Emver
 import           Lib.Types.Emver.Orphans        ( )
 import           Lib.Types.NetAddress
+import qualified Data.HashMap.Strict           as HM
 data AppBase = AppBase
     { appBaseId      :: AppId
     , appBaseTitle   :: Text
@@ -178,3 +179,7 @@ newtype AutoconfigureChangesRes = AutoconfigureChangesRes
     }
 instance ToJSON AutoconfigureChangesRes where
     toJSON AutoconfigureChangesRes {..} = object ["config" .= autoconfigureChangesConfig]
+
+newtype RevertDependencyRes = RevertDependencyRes { revertDependencyChanges :: HM.HashMap AppId Value }
+instance ToJSON RevertDependencyRes where
+    toJSON RevertDependencyRes {..} = object ["changes" .= revertDependencyChanges]
